92170fa6d746b8748bd1030aabe5e73bb428988c
ilangkhumanan <>
2022-02-18 19:36:40 +0800

    Makkal Kural Open Source Innitiative
    kln

---
+++ news_admin/lib/config/config.dart
@@ -0,0 +1,18 @@
+
+class Config{
+  final String appName = 'Incott'; //app name
+  final String testerPassword = '123456';  //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAAk-SctwQ:APA91bFmSznTDX9y2OjhTDBxwkNJJcxDOz7kREoe3E_kKV6N_IfjagJw0Xtw1V2bTttPMqnOgqx2NZ5xzJUL0Qhzwp_3CVVt9AJNJY0G1CLmVWflQk2ctDQKl1qniqmvq0935aeYzVRS';
+  final String icon = 'assets/images/icon.png'; // app icon
+
+  
+  
+  
+  //don't edit or remove this
+  final List contentTypes = [
+    'image',
+    'video'
+  ];
+}
